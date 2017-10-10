2b397de293a0efa00dde47657e65310062388f78
lufa1217 <nek_noke@yahoo.co.jp>
2017-10-11 06:35:45 +0900

    sourcetreeテスト

---
+++ chap08/check_name/nameCheckForm - コピー.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset = "utf-8">
+<title>フォームの入力処理(POST)</title>
+<link href ="../../css/style.css" rel="stylesheet">
+</head>
+<body>
+<div>
+  <form method="post" action="nameCheck.php">
+  <ul>
+    <li><label>名前：<input type="text" name="name"></label></li>
+    <li><input type="submit" value="送信する"></li>
+  </ul>
+  </form>
+</div>
+</body>
+</html>
